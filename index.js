66d5fb57a81a3383d8a7986ceef9566e4b054596
Zack Enders <enders.zack@gmail.com>
2018-04-25 13:37:38 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var tz = Math.round(panelSize / 2 / Math.tan(Math.PI * 2 / numberOfPanels / 2));
+// or simplified to
+var tz = Math.round(panelSize / 2 / Math.tan(Math.PI / numberOfPanels));
+
+function apothem(width, sides) {
+    return Math.ceil(width / (2 * Math.tan(Math.PI / sides)));
+}
+function magic(width, sides) {
+    return Math.round(width * Math.log(sides));
+}
