e5945b3eb1bb6431a1216bd726f9b791ddabb60b
Zack Enders <enders.zack@gmail.com>
2018-04-25 17:35:03 -0400

    Created display cube. Gave it rotate functionality

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var tz = Math.round(panelSize / 2 / Math.tan(Math.PI * 2 / numberOfPanels / 2));
-// or simplified to
-var tz = Math.round(panelSize / 2 / Math.tan(Math.PI / numberOfPanels));
+// var tz = Math.round(panelSize / 2 / Math.tan(Math.PI * 2 / numberOfPanels / 2));
+// // or simplified to
+// var tz = Math.round(panelSize / 2 / Math.tan(Math.PI / numberOfPanels));
 
-function apothem(width, sides) {
-    return Math.ceil(width / (2 * Math.tan(Math.PI / sides)));
-}
-function magic(width, sides) {
-    return Math.round(width * Math.log(sides));
-}
+// function apothem(width, sides) {
+//     return Math.ceil(width / (2 * Math.tan(Math.PI / sides)));
+// }
+// function magic(width, sides) {
+//     return Math.round(width * Math.log(sides));
+// }
+
+
