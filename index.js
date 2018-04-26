bd9171822fcc8c5c82422f523ad58b98c1104f9a
Zack Enders <enders.zack@gmail.com>
2018-04-25 20:11:31 -0400

    Cleaned up the folder. Added some styl styling

--- index.js
+++ index.js
@@ -8,5 +8,47 @@
 // function magic(width, sides) {
 //     return Math.round(width * Math.log(sides));
 // }
+// var btnContainer = document.getElementById("buttons");
+// var btns = document.getElementsByClassName(".btn");
 
+// for (var i = 0; i < btns.length; i++) {
+// 	document
+// 		.getElementsByClassName(".btn")
+// 		[i].addEventListener("click", function() {
+// 			console.log("active");
+// 			var current = document.getElementsByClassName("active");
+// 			current[0].className = current[0].className.replace(" active", "");
+// 			this.className += " active";
+// 		});
+// }
 
+function home() {
+	document.getElementById("cube").style.transform =
+		"rotateX(-15deg) rotateY(15deg)";
+	document.getElementById("container").style.transform =
+		"translate(-50%, -50%)";
+}
+
+function aboutMe() {
+	document.getElementById("cube").style.transform = "rotateY(-90deg)";
+	document.getElementById("container").style.transform =
+		"translate(-110%, -48%)";
+}
+
+function contact() {
+	document.getElementById("cube").style.transform = "rotateY(90deg)";
+	document.getElementById("container").style.transform =
+		"translate(-110%, -48%)";
+}
+function project1() {
+	document.getElementById("cube").style.transform = "rotateX(-90deg)";
+	document.getElementById("container").style.transform = "translate(10%, -48%)";
+}
+function project2() {
+	document.getElementById("cube").style.transform = "rotateX(90deg)";
+	document.getElementById("container").style.transform = "translate(10%, -48%)";
+}
+function project3() {
+	document.getElementById("cube").style.transform = "rotateY(180deg)";
+	document.getElementById("container").style.transform = "translate(10%, -48%)";
+}
