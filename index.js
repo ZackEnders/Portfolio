10e1948fb9ead58136c975911f77517a075b7cbf
Zack Enders <enders.zack@gmail.com>
2018-05-03 11:09:44 -0400

    adjusted the margin for the about me box

--- index.js
+++ index.js
@@ -1,36 +1,3 @@
-// var tz = Math.round(panelSize / 2 / Math.tan(Math.PI * 2 / numberOfPanels / 2));
-// // or simplified to
-// var tz = Math.round(panelSize / 2 / Math.tan(Math.PI / numberOfPanels));
-
-// function apothem(width, sides) {
-//     return Math.ceil(width / (2 * Math.tan(Math.PI / sides)));
-// }
-// function magic(width, sides) {
-//     return Math.round(width * Math.log(sides));
-// }
-// var btnContainer = document.getElementById("buttons");
-// var btns = document.getElementsByClassName(".btn");
-
-// for (var i = 0; i < btns.length; i++) {
-// 	document
-// 		.getElementsByClassName(".btn")
-// 		[i].addEventListener("click", function() {
-// 			console.log("active");
-// 			var current = document.getElementsByClassName("active");
-// 			current[0].className = current[0].className.replace(" active", "");
-// 			this.className += " active";
-// 		});
-// }
-// function logo() {
-// 	document.getElementById("logo").style.visibility = "visible";
-// 	document.getElementById("logo").style.transform = "translate(-500px, 10px)";
-// }
-// var aboutDisplay = document.getElementById("about").style.visibility;
-// var contactDisplay = document.getElementById("contact").style.visibility;
-// var project1Display = project1.style.visibility;
-// var project2Display = project2.style.visibility;
-// var project3Display = project3.style.visibility;
-
 //Checks in the content box is displayed
 function displayed() {
 	switch ("block") {
